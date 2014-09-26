05e31a17905adee141d8b7ca637364990c36332f
Thiago Borges <borges@helabs.com.br>
2014-09-26 16:04:24 -0300

    Add autoprefixer example

--- 2-sass-coffee-autoprefixer/gulpfile.js
+++ 2-sass-coffee-autoprefixer/gulpfile.js
@@ -1,16 +1,25 @@
 var gulp = require('gulp')
   , sass = require('gulp-sass')
   , coffee = require('gulp-coffee')
+  , autoprefixer = require('gulp-autoprefixer')
 ;
 
 gulp.task('sass', function () {
-  gulp.src('./_sass/style.scss')
+  return gulp.src('./_sass/style.scss')
     .pipe(sass())
     .pipe(gulp.dest('./css'));
 });
 
-gulp.task('coffee', function(){
-  gulp.src('./coffeescript/*.coffee')
+gulp.task('coffee', function() {
+  return gulp.src('./coffeescript/*.coffee')
     .pipe(coffee())
     .pipe(gulp.dest('./javascript'));
+})
+
+gulp.task('prefixer', function() {
+  return gulp.src('css/*.css')
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('css'));
 })
