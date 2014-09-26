68e79158329aa340d0a21f281fa0c47d1f8a3c1b
Thiago Borges <borges@helabs.com.br>
2014-09-26 01:44:49 -0300

    Rename minified css

--- 1-concat-minify-uglify/gulpfile.js
+++ 1-concat-minify-uglify/gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp')
-  , minifyCSS = require('gulp-minify-css');
+  , minifyCSS = require('gulp-minify-css')
+  , rename = require('gulp-rename')
+;
 
 gulp.task('minify-css', function() {
   gulp.src('css/style.css')
     .pipe(minifyCSS())
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('assets'))
 });
