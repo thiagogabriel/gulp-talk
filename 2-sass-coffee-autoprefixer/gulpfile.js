9d33334e6e091cdd1820341f2b5a4e216c03d787
Thiago Borges <borges@helabs.com.br>
2014-09-26 02:22:19 -0300

    Add script to create css based on sass file

--- 2-sass-coffee-autoprefixer/gulpfile.js
+++ 2-sass-coffee-autoprefixer/gulpfile.js
@@ -1,4 +1,8 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass')
 
 gulp.task('sass', function () {
+    gulp.src('./_sass/style.scss')
+      .pipe(sass())
+      .pipe(gulp.dest('./css'));
 });
