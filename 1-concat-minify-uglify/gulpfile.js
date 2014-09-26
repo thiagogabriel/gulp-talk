b5c1aa4910246fd5604a6301dfe42142ef445866
Thiago Borges <borges@helabs.com.br>
2014-09-26 02:00:41 -0300

    Concat css

--- 1-concat-minify-uglify/gulpfile.js
+++ 1-concat-minify-uglify/gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp')
   , minifyCSS = require('gulp-minify-css')
+  , concat = require('gulp-concat')
   , rename = require('gulp-rename')
 ;
 
 gulp.task('minify-css', function() {
-  gulp.src('css/style.css')
+  gulp.src('css/*.css')
+    .pipe(concat('style.css'))
     .pipe(minifyCSS())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('assets'))
